7adcb5797c8e935d27f3041452b58cf4e7daba33
asafshtein <asafshtein@gmail.com>
2026-06-27 20:04:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "build-system-v16";
+const CACHE_NAME = "build-system-v17";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
